08cfc482b9fb4cf1e97c304ba3e7b252db0bab09
hiraeth31 <beautiful_distress@outlook.com>
2025-10-01 23:17:05 +1000

    feat: add category

---
+++ LetterBox.Backend/src/LetterBox.Infrastructure/Repositories/CategoriesRepository.cs
@@ -0,0 +1,31 @@
+﻿using LetterBox.Application.Categories;
+using LetterBox.Domain.ArticlesManagement;
+
+namespace LetterBox.Infrastructure.Repositories
+{
+    public class CategoriesRepository : ICategoriesRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoriesRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Guid> Add(Category category, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await _dbContext.Categories.AddAsync(category, cancellationToken);
+
+                await _dbContext.SaveChangesAsync(cancellationToken);
+
+                return category.Id;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
